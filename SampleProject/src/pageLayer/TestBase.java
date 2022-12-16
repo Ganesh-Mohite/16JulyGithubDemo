e570605b5201cf7e9a3477676030b462533a26f4
Ganesh-Mohite <120720282+Ganesh-Mohite@users.noreply.github.com>
2022-12-16 17:10:57 +0530

    Basic Commit
    Hi All,
    I have created project on github.com
    Go ahead & Clone it. start with your respective tasks

---
+++ SampleProject/src/pageLayer/TestBase.java
@@ -0,0 +1,14 @@
+package pageLayer;
+
+public class TestBase {
+
+	
+	public static void main(String[]args) {
+		
+	System.out.println("Hello Github");	
+	System.out.println("Test Base Class");
+	System.out.println("Added dependencies in pom.xml file");
+		
+	}
+	
+  }
